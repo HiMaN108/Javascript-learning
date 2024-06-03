3f2a116abf01d93c7faee188245b017ee8765f3d
Himanshu <him2000.maurya@gmail.com>
2024-06-03 10:23:32 +0530

    till math and number

---
+++ 01_bascis/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+const hund= 10000000;
+
+// console.log(hund.toLocaleString('en-IN'));
+
+//maths ke bare me
+
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6));
+
+
+const min =10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min +1)) + min);
+
+
+console.log()
